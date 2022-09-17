f1952b0d1e3977979313d5bb5a875515beb20d04
Neil <neilpshukla221203@gmail.com>
2022-09-17 17:15:48 +0530

    _blank warning resolved

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
     <div className="header-content ">
         <img style ={{borderRadius : '100%', height : '3rem', width : '3rem'}}src={require("../headerimg.jpg")} alt="icon" />
         <a style={TextStyle} href = "/">My Gallery </a>
-        <a style = {{float : 'right'}} target = "_blank" href="https://github.com/N-E-I-L-S/">
+        <a style = {{float : 'right'}} target = "_blank" rel="noreferrer" href="https://github.com/N-E-I-L-S/">
           <img style = {{borderRadius : '100%', height : '2.5rem', width : '2.5rem'}}src={require("../githubicon.png")} alt="My Github" />
         </a>
     </div>
